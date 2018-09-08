92bb2a9e2cf3487470f5c6b825b5de46f9a401d6
viktorfa <vikfand@gmail.com>
2018-09-08 21:37:50 +0200

    Print number of products in main method.

--- receipt_scanning/main.py
+++ receipt_scanning/main.py
@@ -9,7 +9,15 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 from receipt_scanning.Receipt import Receipt
 from receipt_scanning.lib import read_receipt_from_google_ocr_json
 
-with open('./receipt_scanning/tests/data/receipt_keiser.json') as json_file:
-  receipt = read_receipt_from_google_ocr_json(json.load(json_file))
+with open('./receipt_scanning/tests/data/receipt_kiwi.json') as json_file:
+  receipt = read_receipt_from_google_ocr_json(json.load(json_file), 0)
 
-print(receipt.token_lines)
+
+products = receipt.get_all_products()
+print('Number of products found: {}'.format(len(products)))
+
+try:
+  with open('outfile.json', 'w') as outfile:
+    json.dump([vars(x) for x in products], outfile)
+finally:
+  outfile.close()
