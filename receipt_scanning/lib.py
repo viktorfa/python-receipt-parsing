1a5ab0cf7db2c0bdbb6d771e00c340a83cbc43d4
viktorfa <vikfand@gmail.com>
2018-09-09 23:04:24 +0200

    Improved method of finding overlaps (lines).

--- receipt_scanning/lib.py
+++ receipt_scanning/lib.py
@@ -1,27 +1,52 @@
 import json
 from functools import partial
+from operator import itemgetter
 
 from receipt_scanning.Receipt import Receipt
 
-def read_receipt_from_google_ocr_json(json_dict, OVERLAP_SAME_LINE_LIMIT=20):
+def read_receipt_from_google_ocr_json(json_dict):
     text_annotations = json_dict['responses'][0]['textAnnotations']
-    full_text_with_y_axis = [(annotation['description'], (annotation['boundingPoly']['vertices'][0]['y'], annotation['boundingPoly']['vertices'][2]['y'])) for annotation in text_annotations]
-    overlap_map = get_overlap_map(full_text_with_y_axis, OVERLAP_SAME_LINE_LIMIT)
+    optimal_height_factor, _ = find_optimal_height_factor(text_annotations)
+    overlap_map = get_overlap_map(text_annotations, optimal_height_factor / 10)
     distinct_overlaps = get_distinct_lists(overlap_map)
     sorted_distinct_overlaps = sort_overlap_groups_by_x_axis(distinct_overlaps, text_annotations)
     return Receipt(sorted_distinct_overlaps, text_annotations)
+
+def find_optimal_height_factor(annotations):
+    height_factors = range(1, 17, 2)
+    results = []
+    for hf in height_factors:
+        results.append((hf, len(get_distinct_lists(get_overlap_map(annotations, hf / 10))))) 
+    return min(results, key=itemgetter(1))
     
-    
-def get_overlap_map(full_text_with_y_axis, OVERLAP_SAME_LINE_LIMIT=20):
-    result = [[] for _ in full_text_with_y_axis]
-    for i, (_, a) in enumerate(full_text_with_y_axis):
-        for j, (_, b) in enumerate(full_text_with_y_axis):
-            overlap_size = get_overlap_size(a, b)
-            if overlap_size >= OVERLAP_SAME_LINE_LIMIT:
+def get_overlap_map(annotations, height_factor=.6):
+    result = [[] for _ in annotations]
+    for i, a in enumerate(annotations):
+        a_y_center = get_y_center(a['boundingPoly'])
+        a_height = get_y_height(a['boundingPoly'])
+        for j, b in enumerate(annotations):
+            b_y_center = get_y_center(b['boundingPoly'])
+            b_height = get_y_height(b['boundingPoly'])
+            
+            ab_dist_center = abs(a_y_center - b_y_center)
+            ab_min_height = min(a_height, b_height)
+            ab_max_height = max(a_height, b_height)
+            
+            ab_is_same_line = ab_dist_center < ab_min_height * height_factor and ab_max_height < ab_min_height * 5
+            
+            if ab_is_same_line:
                 result[i].append(j)
-                
     return result
-    
+
+def get_y_height(ocr_poly):
+    return abs(ocr_poly['vertices'][0]['y'] - ocr_poly['vertices'][3]['y'])
+
+def get_y_center(ocr_poly):
+    return sum([p[1] for p in get_poly_from_ocr_poly(ocr_poly)]) / 4
+
+def get_poly_from_ocr_poly(ocr_poly):
+    return ((p['x'], p['y']) for p in ocr_poly['vertices'])
+
 def get_height(a):
     return a[1] - a[0]
 
