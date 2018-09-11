4c2285d0e561db2c5f515b956df128d542587c9d
viktorfa <vikfand@gmail.com>
2018-09-11 16:00:27 +0200

    Move and update main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+import sys
+import os
+import time
+
+from gcloud.ocr import get_ocr_response_from_image_file, get_ocr_response_from_url
+from receipt_scanning.lib import read_receipt_from_google_ocr_json
+
+start_time = time.time()
+
+
+receipt = read_receipt_from_google_ocr_json(get_ocr_response_from_url('gs://vfa-receipt-scanning_receipt-images/kvittering-kiwi.jpg'))
+
+products = receipt.get_all_products()
+print('Number of products found: {}'.format(len(products)))
+print('Executed in {} seconds'.format(time.time() - start_time))
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'output', 'outfile.json'), 'w') as outfile:
+        json.dump([vars(x) for x in products], outfile, indent=2)
+finally:
+    outfile.close()
