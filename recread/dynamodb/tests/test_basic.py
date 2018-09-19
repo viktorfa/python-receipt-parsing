a01c0debe7a885d952fc8833c8a79a0769d77ba2
viktorfa <vikfand@gmail.com>
2018-09-19 16:00:03 +0200

    Fix broken test

--- recread/dynamodb/tests/test_basic.py
+++ recread/dynamodb/tests/test_basic.py
@@ -1,9 +1,10 @@
 import unittest
-
+from unittest.mock import patch
 from recread.dynamodb.core import save_gcv_response
 
 class TestBasic(unittest.TestCase):
-  def test_can_save(self):
+  @patch('recread.dynamodb.core.GcvResponseModel')
+  def test_can_save(self, GcvResponseModelMock):
     gcv_response = dict(hei='per')
     image_hash = '123abc'
     actual = save_gcv_response(gcv_response, image_hash)
